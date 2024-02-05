9fc60e1aa4dcc4e8920477d4a933c6d0c163b088
filiperak <filip.erak@googlemail.com>
2024-02-05 18:42:18 +0100

    drugicas

--- 28_CLOUD_FIRESTORE/script4.js
+++ 28_CLOUD_FIRESTORE/script4.js
@@ -19,10 +19,43 @@ db.collection('users')
 */
 
 //DOHVATANJE ODREDJENOG BROJA DOKUMENATA (LIMITIRANJE)
-
+/*
 db.collection('users')
-//.orderBy('name','desc')
-//.orderBy('age','asc')
+.orderBy('name','desc')
+.orderBy('age','asc')
+.limit(2)
+.get()
+.then(snapshot => {
+    snapshot.forEach(elem => {
+        let data = elem.data()
+        console.log(data);
+    })
+})
+.catch(err => {
+    console.log(`error: ${err}`);
+});
+*/
+//DOHVATANJE DOKUMENATA KOJI ZADOVOLJAVAJU ODREDJENE USLOVE (FILTRIRANJE)
+//dohvati seve klijente koji imaju platu vecu ili jednako 300
+
+db.collection('customers')
+//.where('age','<',22)
+.where('salary','>=',300)
+.get()
+.then(snapshot => {
+    snapshot.forEach(elem => {
+        let data = elem.data()
+        console.log(data);
+    })
+})
+.catch(err => {
+    console.log(`error: ${err}`);
+});
+
+// platu izmedju 100 i 200
+db.collection('customers')
+.where('salary','>=',100)
+.where('salary','<=',300)
 .get()
 .then(snapshot => {
     snapshot.forEach(elem => {
