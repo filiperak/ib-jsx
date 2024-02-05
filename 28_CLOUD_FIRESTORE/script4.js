51056682a2d9506547cda352a2b8555d3d39baf6
filiperak <filip.erak@googlemail.com>
2024-02-05 20:17:56 +0100

    cas2

--- 28_CLOUD_FIRESTORE/script4.js
+++ 28_CLOUD_FIRESTORE/script4.js
@@ -100,7 +100,7 @@ db.collection('customers')
     console.log(`error: ${err}`);
 });
 
-
+/*
 db.collection('customers')
 .where('adresses','array-contains','nis')
 .get()
@@ -113,4 +113,114 @@ db.collection('customers')
 .catch(err => {
     console.log(`error: ${err}`);
 });
+*/
 
+
+db.collection('customers')
+.where('adresses','array-contains-any',['nis','adress1'])
+.get()
+.then(snapshot => {
+    snapshot.forEach(elem => {
+        let data = elem.data()
+        console.log(data);
+    })
+})
+.catch(err => {
+    console.log(`error: ${err}`);
+});
+
+
+db.collection('customers')
+.where('age','in',[21,28])
+.get()
+.then(snapshot => {
+    snapshot.forEach(elem => {
+        let data = elem.data()
+        console.log(data);
+    })
+})
+.catch(err => {
+    console.log(`error: ${err}`);
+});
+//////////////////////////////////////////////
+
+db.collection('tasks')
+.where('priority','==',true)
+.get()
+.then(snapshot => {
+    snapshot.forEach(elem => {
+        let data = elem.data()
+        console.log(data);
+    })
+})
+.catch(err => {
+    console.log(`error: ${err}`);
+});
+//Treba da se izvrše u tekućoj godini,
+//Su završeni,
+//Tek treba da počnu.
+
+let datum = new Date();
+let godine = datum.getFullYear();
+let datum1 = new Date(godine,0,1);
+let datum2 = new Date(godine+1,0,1);
+let ts1 = firebase.firestore.Timestamp.fromDate(datum1);
+let ts2= firebase.firestore.Timestamp.fromDate(datum2);
+
+
+db.collection('tasks')
+.where('due_date','>=',ts1)
+.where('due_date','<',ts2)
+.get()
+.then(snapshot => {
+    snapshot.forEach(elem => {
+        let data = elem.data()
+        console.log(data);
+    })
+})
+.catch(err => {
+    console.log(`error: ${err}`);
+});
+
+/////////////////////////////////////////////////////////
+/*
+db.collection('movies')
+.where('rating','<',9)
+.where('rating','>',7)
+.get()
+.then(snapshot => {
+    snapshot.forEach(elem => {
+        let data = elem.data()
+        console.log(data);
+    })
+})
+.catch(err => {
+    console.log(`error: ${err}`);
+});
+
+db.collection('movies')
+.where('genres','array-contains-any',['comedy','action'])
+.get()
+.then(snapshot => {
+    snapshot.forEach(elem => {
+        let data = elem.data()
+        console.log(data);
+    })
+})
+.catch(err => {
+    console.log(`error: ${err}`);
+});
+
+db.collection('movies')
+.where('genres','array-contains-any',['comedy','action'])
+.get()
+.then(snapshot => {
+    snapshot.forEach(elem => {
+        let data = elem.data()
+        console.log(data);
+    })
+})
+.catch(err => {
+    console.log(`error: ${err}`);
+});
+*/
