04936e22ca151c0c5a9bf29433299d34fa59ad59
filiperak <filip.erak@googlemail.com>
2024-02-05 18:20:24 +0100

    CAS 1 COMMIT

--- 28_CLOUD_FIRESTORE/script4.js
+++ 28_CLOUD_FIRESTORE/script4.js
@@ -1,3 +1,8 @@
+//dohvatanje dokumenata iz kolekcije po redosledu polja
+//dokument mora da sadrzi to polje da bi bio dohvacen
+//ako ima vise od jednog polja neophodno je prvo kreirati index
+
+/*
 db.collection('users')
 .orderBy('name','desc')
 .orderBy('age','asc')
@@ -11,4 +16,20 @@ db.collection('users')
 .catch(err => {
     console.log(`error: ${err}`);
 });
+*/
 
+//DOHVATANJE ODREDJENOG BROJA DOKUMENATA (LIMITIRANJE)
+
+db.collection('users')
+//.orderBy('name','desc')
+//.orderBy('age','asc')
+.get()
+.then(snapshot => {
+    snapshot.forEach(elem => {
+        let data = elem.data()
+        console.log(data);
+    })
+})
+.catch(err => {
+    console.log(`error: ${err}`);
+});
