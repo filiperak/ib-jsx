9dd4f4302d03ea2d9ad2beb3c545c9f96bb8d8a4
filiperak <filip.erak@googlemail.com>
2024-02-08 21:06:39 +0100

    cas4

--- 30_PROJEKAT/app.js
+++ 30_PROJEKAT/app.js
@@ -1,26 +1,33 @@
 import {Chatroom} from './chats.js';
 import { chatUI } from './ui.js';
 
-const messageUl = document.querySelector('.msgUl')
 let chatroom1 = new Chatroom('homeworks','filip01')
 chatroom1.room = 'JS'
 //chatroom1.addChat('poruka 123 test')
 
-chatroom1.getChats((data) => {
-    console.log(data.created_at.toDate());
-    console.log(data);
-    templateLI(data)
-})
+//chatroom1.getChats((data) => {
+//    console.log(data.created_at.toDate());
+//    console.log(data);
+//    templateLI(data)
+//})
 
-const templateLI = (dt) => {
-    let liElem = document.createElement('li');
-    liElem.innerHTML = `${dt.username}: ${dt.message} <br> ${formatDate(dt.created_at.toDate())}`
-    messageUl.appendChild(liElem)
-}
+//const templateLI = (dt) => {
+//    let liElem = document.createElement('li');
+//    liElem.innerHTML = `${dt.username}: ${dt.message} <br> ${formatDate(dt.created_at.toDate())}`
+//    //messageUl.appendChild(liElem)
+//    return liElem;
+//}
 
-const formatDate = (date) => `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}. ${date.getHours()}:${date.getMinutes()}`
+//const formatDate = (date) => `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}. ${date.getHours()}:${date.getMinutes()}`
 
 /*
 let x = 88
 console.log(String(x).padStart(3,'0'));
 */
+const messageUl = document.querySelector('.msgUl')
+
+let chatUI1 = new chatUI(messageUl)
+chatroom1.getChats(data => {
+    //chatUI1.list.appendChild(chatUI1.tamplateLI(data));
+    chatUI1.tamplateLI(data)
+})
--- 30_PROJEKAT/ui.js
+++ 30_PROJEKAT/ui.js
@@ -8,4 +8,12 @@ export class chatUI {
     get list(){
         return this._list
     }
+    formatDate = (date) => `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}. ${date.getHours()}:${date.getMinutes()}`
+
+    tamplateLI(dt){
+        let liElem = document.createElement('li');
+        liElem.innerHTML = `${dt.username}: ${dt.message} <br> ${this.formatDate(dt.created_at.toDate())}`
+        //return liElem;
+        return this.list.append(liElem)
+    }
 }
