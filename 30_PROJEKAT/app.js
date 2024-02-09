9ecf735fb7a71dc268f8333eccb58d2efb7ad375
filiperak <filip.erak@googlemail.com>
2024-02-09 18:58:37 +0100

    cas2

--- 30_PROJEKAT/app.js
+++ 30_PROJEKAT/app.js
@@ -4,8 +4,21 @@ import { chatUI } from './ui.js';
 const messageUl = document.querySelector('.msgUl');
 const messageInp = document.querySelector('#msg');
 const formMsg = document.querySelector('#inputMsg');
+const formUpdate = document.querySelector('#inputUpdate');
+const usernameInp = document.querySelector('#username');
+
+const getUserData = () => {
+    if(localStorage.getItem('user') == null){
+        return localStorage.setItem('user','anonymus')
+    }else{
+        return localStorage.getItem('user')
+    }
+}
+
+let usernameStorage = getUserData();
+
 //OBJEKTI
-let chatroom1 = new Chatroom('homeworks','filip01')
+let chatroom1 = new Chatroom('homeworks',usernameStorage)
 let chatUI1 = new chatUI(messageUl);
 //PRIKAZ PORUKA NA STRANICI
 chatroom1.getChats(data => {
@@ -19,4 +32,12 @@ formMsg.addEventListener('submit',(e) => {
         chatroom1.addChat(msgVal);
     } 
     formMsg.reset();
+});
+
+formUpdate.addEventListener('submit',(e) => {
+    e.preventDefault();
+    let usernameVal = usernameInp.value
+    chatroom1.username = usernameVal;
+    localStorage.setItem('user',usernameVal)
+    formUpdate.reset();
 });
