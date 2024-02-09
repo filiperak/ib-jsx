37ba99cb21632f1e77152d3f887755412fe37a39
filiperak <filip.erak@googlemail.com>
2024-02-09 17:48:17 +0100

    2.9-1

--- 30_PROJEKAT/app.js
+++ 30_PROJEKAT/app.js
@@ -1,14 +1,22 @@
 import {Chatroom} from './chats.js';
 import { chatUI } from './ui.js';
 //DOM
-const messageUl = document.querySelector('.msgUl')
-
+const messageUl = document.querySelector('.msgUl');
+const messageInp = document.querySelector('#msg');
+const formMsg = document.querySelector('#inputMsg');
 //OBJEKTI
 let chatroom1 = new Chatroom('homeworks','filip01')
 let chatUI1 = new chatUI(messageUl);
-
 //PRIKAZ PORUKA NA STRANICI
 chatroom1.getChats(data => {
-    //chatUI1.list.appendChild(chatUI1.tamplateLI(data));
     chatUI1.tamplateLI(data)
-})
+});
+
+formMsg.addEventListener('submit',(e) => {
+    e.preventDefault();
+    let msgVal = messageInp.value;
+    if(msgVal.trim() != 0){
+        chatroom1.addChat(msgVal);
+    } 
+    formMsg.reset();
+});
