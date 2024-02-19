bdbf7d55ec1acd734d1b1866b4277000e27a49fd
filiperak <filip.erak@googlemail.com>
2024-02-19 20:25:07 +0100

    cas1

--- prvi-cas/src/App.js
+++ prvi-cas/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hallo World</h1>
+      <h1 style={{color:'red'}}>Hallo World</h1>
     </div>
   );
 }
