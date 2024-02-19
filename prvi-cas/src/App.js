5fdad8259052a06bc981c9fe96aa7b8483203808
filiperak <filip.erak@googlemail.com>
2024-02-19 20:33:28 +0100

    stil

--- prvi-cas/src/App.js
+++ prvi-cas/src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
 
 function App() {
+  const myStyle = {
+    container: {
+      backgroundColor:'lightblue',
+      padding: '20px'
+    },
+    heading: {
+      color:'green',
+      fontSize:'24px'
+    },
+  };
   return (
     <div className="App">
-      <h1 style={{color:'red'}}>Hallo World</h1>
+      <h1 
+        style={{color:'red',
+          border:'1px solid blue',
+          borderRadius:'10px',
+          width:'fit-content'
+        }}
+      >Hallo World
+      </h1>
+      <div style={myStyle.container}>
+        <p style={myStyle.heading}>Welcome to my app</p>
+      </div>
     </div>
   );
 }
