bd23bb6d300a51cbdb37fc48bc109074b9f6cd72
filiperak <filip.erak@googlemail.com>
2024-02-22 19:25:03 +0100

    cas1

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Counter from './components/Counter';
+import { useState } from 'react';
 
 function App() {
+  const [show, setShow] = useState(true);
   return (
     <div className="App">
-      <Counter/>
+      {/* {show && <Counter/>} ako je show tru ispisi mi counter, radi isto kao linija ispod */}
+      {show? <Counter/> : null}
+      <button onClick={() => setShow(!show)}>{show? 'hide':'show'}</button>
     </div>
   );
 }
--- hooks/src/components/Counter.jsx
+++ hooks/src/components/Counter.jsx
@@ -8,9 +8,22 @@ const Counter = () => {
     const [counter, setCounter] = useState(0);
     //const [counter, setCounter] = useState(initialFunc());
 
+    // useEffect(() => {           //RENDERUJE SE PRI SVAKOJ PROMENI
+    //     console.log('render');
+    // });
+
+    useEffect(() => {       //SAMO JEDNOM KADA SE KOMPONENTA UPISALA U DOM
+        console.log('commponent did mount');
+    },[0]);
     useEffect(() => {
-        console.log('render');
-    })
+        console.log('component did update');
+    },[counter])
+    //KADAA SE KOMPONENTA IZBRISE IZ DOMA
+    useEffect(() => {
+        return () => {
+            console.log('component did unmounnt');
+        }
+    },[])
     return(
         <>
             <button onClick={() => setCounter(counter - 1)}>-</button>
