78dc1e49b937fa39f235b10e976d62f6073ecd1f
filiperak <filip.erak@googlemail.com>
2024-02-26 21:12:24 +0100

    cas3

--- react-router-dom/src/App.js
+++ react-router-dom/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
+import {BrowserRouter as Router, Routes,Route, Link} from "react-router-dom"
+import {Home, Users , About} from "./components"
+import UserContextProvider from './context/UserContext';
+
 function App() {
   return (
     <div className="App">
-      
+      <UserContextProvider>
+        <Router>
+          <div className='navbar'>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/users'>Users</Link>
+          </div>
+          <Routes>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/users' element={<Users/>}/>
+            <Route path='/' element={<Home/>}/>
+          </Routes>
+        </Router>
+      </UserContextProvider>
     </div>
   );
 }
--- react-router-dom/src/components/Users.jsx
+++ react-router-dom/src/components/Users.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 const Users = () => {
-    return(
-        <h1>USERS</h1>
-    )
+  const { users, dispatch } = useContext(UserContext);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => dispatch({
+        type: 'SET_ALL_USERS',
+        payload: json
+      }))
+  }, []);
+
+  return (
+    <>
+      <h1>USERS</h1>
+      {users.map(user => (
+        <p key={user.id}>
+          #{user.id}
+        </p>
+      ))}
+    </>
+  );
 }
+
 export default Users;
