30340d94cb80ee66f6620d45c2f1b40bc0c53dd2
filiperak <filip.erak@googlemail.com>
2024-02-27 18:23:44 +0100

    cas1

--- react-router-dom/src/components/UserItem.jsx
+++ react-router-dom/src/components/UserItem.jsx
@@ -6,16 +6,23 @@ const UserItem = () => {
     const {userId} = useParams();
     const {users} = useContext(UserContext);
     const [user,setUser] = useState(undefined);
-
+    const navigate = useNavigate();
     useEffect(() => {
         const userE = users.find(user => user.id === parseInt(userId))
         setUser(userE)
     },[]);
-
+    const handleHomeButton = () => {
+        navigate('/')
+    }
+    const handleBackButton = () => {
+        navigate(-1)
+    }
 
     return( 
         <>
             <h1>User item {userId} </h1>
+            <button onClick={handleHomeButton}>Home</button>
+            <button onClick={handleBackButton}>Back</button>
             {user? (
                 <div>
                 <p>name: {user.name}</p>
